e1a79e99f64c629ea69dae4e2789264eff5cbd3d
ev001 <cho.evergreen@gmail.com>
2020-02-25 21:09:11 +0900

    문제 번호 수정

--- 1080.py
+++ 1080.py
@@ -1,37 +1,29 @@
+# 크기 제한이 20억이다.
+# for문으로는 불가.
+# 수식을 이용한다.
+
 import sys
 
 input = sys.stdin.readline
 
-N = list(input().strip())
 
-if sum(map(int, N)) % 3 == 0 and "0" in N:
-    print("".join(sorted(N, reverse=True)))
+def flip(i, j):
+    for x in range(i, i + 3, 1):
+        for y in range(j, j + 3, 1):
+            a[x][y] = 1 - a[x][y]
 
-else:
-    print(-1)
 
-'''
-     
-    
-# for i in range(len(N)):
-#     value += N[i] * pow(10, (len(N) - (i + 1)))
-# if value % 30 == 0:
-#     print(value)
-# else:
-#     print(-1)
+N, M = map(int, input().split())
 
-    처음에는 위와 같이 숫자를 제곱해서 문제를 해결하고자 하였음.
-    그랬더니 시간초과 발생.
-    
-    파이썬의 장점인 문자열을 이용해 문제를 풀고자함.
-    
-    고려한것 
-    1. 3의 배수 판정법 
-    2. 각 자리 수는 0~9 범위. 따라서 str 상태로 sort OK!   
-    
-    어려웠던 점.
-    1. join 함수    
-        join은 문자열 관련 함수인데 int 형으로 list를 만들어
-        계속 오류를 발생
-    2. 그외 파이썬 빌트인 함수들 .. 
-'''
+a = [list(map(int, input().strip())) for _ in range(N)]
+b = [list(map(int, input().strip())) for _ in range(N)]
+
+ans = 0
+# 3*3 의 부분행렬의 범위는 전체 N-2, M-2 이다.
+for i in range(N - 2):
+    for j in range(M - 2):
+        if a[i][j] != b[i][j]:
+            ans += 1
+            flip(i, j)
+
+print(ans if sorted(a) == sorted(b) else -1)
