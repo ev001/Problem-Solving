94732e4cb4565352e59638644a761136fd933d8c
ev001 <cho.evergreen@gmail.com>
2020-03-01 20:06:51 +0900

    Second commit

--- 2667.py
+++ 2667.py
@@ -1,40 +1,51 @@
 # 단지번호붙이기 # BFS
 import sys
-import collections
+from collections import deque
 
 input = sys.stdin.readline
 
 
-def bfs(length):
-    q = collections.deque()
-    cnt = 1
-    for i in range(length):
-        for j in range(length):
-            q.append(a[i, j])  # [y,x] 좌표
-            node = q.popleft()
-            for idx in range(4):
-                x = node[1] + i_x[idx]
-                y = node[0] + i_y[idx]
-                if 0 <= x <= n - 1 and 0 <= y <= n - 1:
-                    if a[y][x] == 1 and visit[y][x] == 0:
-                        q.append(visit[y][x])
-                        visit[y][x] = 1
-            cnt += 1
-    return cnt
+def bfs(y, x, count):
+    q = deque()
+    q.append([y, x])
+    visit[y][x] = count
+    while q:
+        y, x = q.popleft()
+
+        for idx in range(4):
+            ny, nx = y + i_y[idx], x + i_x[idx]
+
+            if 0 <= nx < n and 0 <= ny < n:
+                if a[ny][nx] == 1 and visit[ny][nx] == 0:
+                    q.append([ny, nx])
+                    visit[ny][nx] = count
+    return count
 
 
 n = int(input())
 a = [list(map(int, input().strip())) for _ in range(n)]
 
 # 이동방향 4방향
-i_y = [0, -1, 0, 1]
 i_x = [-1, 0, 1, 0]
-
-# 단지 리스트 d_list[]
-d_list = []
-# 한 단지 세는거 cnt
+i_y = [0, -1, 0, 1]
 
 # 방문했다는 visit[]
 visit = [[0] * n for _ in range(n)]
 
-print(bfs(n))
+# 방문 단지 세는 cnt
+cnt = 0
+
+for i in range(n):
+    for j in range(n):
+        if visit[i][j] == 0 and a[i][j] == 1:
+            cnt += 1
+            bfs(i, j, cnt)
+
+ans = [0] * (n ** 2)
+for i in range(n):
+    for j in range(n):
+        if visit[i][j]:
+            ans[visit[i][j]] += 1
+print(cnt)
+for i in sorted(ans[1:cnt + 1]):
+    print(i)
