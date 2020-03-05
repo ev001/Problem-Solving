31de82a09f1118c44088ef452a7a4e1a8d302952
ev001 <cho.evergreen@gmail.com>
2020-03-05 20:50:57 +0900

    First commit. Try again later.

---
+++ 11729.py
@@ -0,0 +1,13 @@
+# 하노이탑 # 분할정복
+
+def solve(n, x, y):
+    if n == 0:
+        return
+    solve(n - 1, x, 6 - x - y)
+    print(x, y)
+    solve(n - 1, 6 - x - y, y)
+
+
+n = int(input())
+print(2 ** n - 1)
+solve(n, 1, 3)
