c752218f1557971e2081897527534f07a292a95c
ev001 <cho.evergreen@gmail.com>
2020-03-09 17:08:09 +0900

    Add exception case

--- 2579.py
+++ 2579.py
@@ -4,13 +4,20 @@ input = sys.stdin.readline
 
 n = int(input())
 a = [int(input().strip()) for _ in range(n)]
-dp = [0]*n
+dp = [0] * n
 
-dp[0] = a[0]                            # 첫번째 계단
-dp[1] = a[1] + dp[0]                    # 두번째 계단
-dp[2] = max(dp[0] + a[2], a[1] + a[2])  # 세번째 계단
+if n >= 1:
+    dp[0] = a[0]  # 첫번째 계단
+if n >= 2:
+    dp[1] = a[1] + dp[0]  # 두번째 계단
+if n >= 3:
+    dp[2] = max(dp[0] + a[2], a[1] + a[2])
 
 for i in range(3, n):
     dp[i] = max(dp[i - 2] + a[i], a[i] + a[i - 1] + dp[i - 3])
 
-print(dp[n-1])
+print(dp[- 1])
+
+'''
+    계단이 3개 이하일때의 예외 case 추가
+'''
