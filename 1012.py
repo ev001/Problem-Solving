d153505ff72aeec5e88095d66e91086414ef012d
ev001 <cho.evergreen@gmail.com>
2020-03-25 12:58:46 +0900

    Second commit.

--- 1012.py
+++ 1012.py
@@ -1,35 +1,42 @@
-# 유기농 배추 # 그래프
+# 유기농 배추
 import sys
 from collections import deque
 
 input = sys.stdin.readline
 
 tc = int(input())
-# 이 문제는 bfs로 돌수 있는 공간이 몇개 있는지
+dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
 
-dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
-for _ in range(tc):
-    r, c, p = map(int, input().split())
-    arr = [[0] * c for _ in range(r)]
-    visit = [[0] * c for _ in range(r)]
 
-    for i in range(p):
-        rr, cc = map(int, input().split())
-        arr[rr][cc] = 1
-
+def bfs(r, c):
     q = deque()
+    q.append((r, c))
+
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if arr[nx][ny] == 1 and visit[nx][ny] == 0:
+                    visit[nx][ny] = 1
+                    q.append((nx, ny))
+
+
+for _ in range(tc):
+    # m : 가로, n: 세로
+    m, n, k = map(int, input().split())
+
+    arr = [[0] * m for _ in range(n)]
+    visit = [[0] * m for _ in range(n)]
+    for i in range(k):
+        c, r = map(int, input().split())
+        arr[r][c] = 1
+
     cnt = 0
-    for i in range(r):
-        for j in range(c):
+    for i in range(n):
+        for j in range(m):
             if arr[i][j] == 1 and visit[i][j] == 0:
-                q.append((i, j))
-                while q:
-                    x, y = q.popleft()
-                    for k in range(4):
-                        nx, ny = x + dx[k], y + dy[k]
-                        if 0 <= nx < r and 0 <= ny < c:  # 탐색할수있는 좌표면?
-                            if arr[nx][ny] == 1 and visit[nx][ny] == 0:
-                                q.append((nx, ny))
-                                visit[nx][ny] = 1
+                bfs(i, j)
                 cnt += 1
+
     print(cnt)
